ff926227dbe80d2a513c75613fe762684fa09291
Jet Yee <jetyeelow@gmail.com>
2021-05-07 18:26:15 +0800

    retrieve data for profile from cloud firestore

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Profile extends StatefulWidget {
   Profile({Key key, this.title, this.uid}) : super(key: key);
@@ -10,6 +11,7 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
+  final Firestore firestore = Firestore.instance;
   FirebaseUser currentUser;
 
   @override
@@ -20,6 +22,14 @@ class _ProfileState extends State<Profile> {
 
   void getCurrentUser() async {
     currentUser = await FirebaseAuth.instance.currentUser();
+  }
+
+  Future<DocumentSnapshot> getDocument() async {
+    var firebaseUser = await FirebaseAuth.instance.currentUser();
+    return await Firestore.instance
+        .collection("users")
+        .document(firebaseUser.uid)
+        .get();
   }
 
   @override
@@ -38,35 +48,62 @@ class _ProfileState extends State<Profile> {
           ),
         ),
         body: Container(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  'See you again soon !',
-                  style: TextStyle(
-                    fontSize: 24.0,
-                  ),
-                ),
-                RaisedButton(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(40),
-                  ),
-                  child: Text(
-                    'Logout',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  color: Colors.orangeAccent,
-                  onPressed: () {
-                    FirebaseAuth.instance
-                        .signOut()
-                        .then((result) =>
-                            Navigator.pushReplacementNamed(context, "/login"))
-                        .catchError((err) => print(err));
+          child: Column(
+            children: <Widget>[
+              Container(
+                child: FutureBuilder(
+                  future: getDocument(),
+                  builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+                    if (snapshot.connectionState == ConnectionState.done) {
+                      return Text(
+                          snapshot.data["fname"].toString().toUpperCase());
+                    } else if (snapshot.connectionState ==
+                        ConnectionState.none) {
+                      return Text("No data");
+                    }
+                    return CircularProgressIndicator();
                   },
                 ),
-              ],
-            ),
+              ),
+              Container(
+                child: FutureBuilder(
+                  future: getDocument(),
+                  builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+                    if (snapshot.connectionState == ConnectionState.done) {
+                      return Text(
+                          snapshot.data["surname"].toString().toUpperCase());
+                    } else if (snapshot.connectionState ==
+                        ConnectionState.none) {
+                      return Text("No data");
+                    }
+                    return CircularProgressIndicator();
+                  },
+                ),
+              ),
+              Text(
+                'See you again soon !',
+                style: TextStyle(
+                  fontSize: 24.0,
+                ),
+              ),
+              RaisedButton(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(40),
+                ),
+                child: Text(
+                  'Logout',
+                  style: TextStyle(color: Colors.white),
+                ),
+                color: Colors.orangeAccent,
+                onPressed: () {
+                  FirebaseAuth.instance
+                      .signOut()
+                      .then((result) =>
+                          Navigator.pushReplacementNamed(context, "/login"))
+                      .catchError((err) => print(err));
+                },
+              ),
+            ],
           ),
         ));
   }
